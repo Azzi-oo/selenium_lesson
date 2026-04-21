dd7dbec70b49f6c133fce1543e90396c8b39cb8d
azat <azatanberdin02@gmail.com>
2026-04-21 13:55:36 +0500

    add new features

--- lesson_24/js.py
+++ lesson_24/js.py
@@ -6,8 +6,15 @@ from selenium.webdriver.common.action_chains import ActionChains
 driver = webdriver.Chrome()
 action = ActionChains(driver)
 
-driver.get("")
+driver.get("https://seiyria.com/bootstrap-slider/")
+
+# driver.execute_script("alert('Hello)")
+
+
 
 EX_2_LOCATOR = ("xpath", "//h3[text()='Example 2: ']")
 EX_2 = driver.find_element(*EX_2_LOCATOR)
 
+action.scroll_to_element(EX_2).perform()
+
+time.sleep(5)
